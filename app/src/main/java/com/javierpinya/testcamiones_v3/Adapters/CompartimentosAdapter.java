b1c63f4a8be5febdc949a111bc4f206daeebf1f8
Javier <javierpinya@protonmail.com>
2019-12-27 21:52:29 +0100

    Continuar con ResultadoBuscarCisternaFragment. Terminar el RecyclerView

---
+++ app/src/main/java/com/javierpinya/testcamiones_v3/Adapters/CompartimentosAdapter.java
@@ -0,0 +1,77 @@
+package com.javierpinya.testcamiones_v3.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.javierpinya.testcamiones_v3.Clases.TplcprtEntity;
+import com.javierpinya.testcamiones_v3.R;
+
+import java.util.List;
+
+public class CompartimentosAdapter extends RecyclerView.Adapter<CompartimentosAdapter.ViewHolder> {
+
+    private List<TplcprtEntity> compartimentos_list;
+    private int layout;
+    private OnItemClickListener listener;
+
+    public CompartimentosAdapter(List<TplcprtEntity> compartimentos_list, int layout, OnItemClickListener listener){
+        this.compartimentos_list = compartimentos_list;
+        this.layout = layout;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(layout,parent,false);
+        RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(layoutParams);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(compartimentos_list.get(position), listener);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return compartimentos_list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView numcomp,capacidad,tag;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            numcomp = itemView.findViewById(R.id.comp1);
+            capacidad = itemView.findViewById(R.id.capacidad1);
+            tag = itemView.findViewById(R.id.tag1);
+        }
+
+        public void bind(final TplcprtEntity compartimentos, final OnItemClickListener listener){
+            this.numcomp.setText(compartimentos_list.get(getAdapterPosition()).getCod_compartimento());
+            this.capacidad.setText(compartimentos_list.get(getAdapterPosition()).getCan_capacidad());
+            this.tag.setText(compartimentos_list.get(getAdapterPosition()).getCod_tag_cprt());
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(compartimentos, getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(TplcprtEntity compartimentosList, int position);
+    }
+}
--- app/src/main/java/com/javierpinya/testcamiones_v3/ui/ResultadoBuscarVehiculo/ResultadoBuscarCisternaFragment.java
+++ app/src/main/java/com/javierpinya/testcamiones_v3/ui/ResultadoBuscarVehiculo/ResultadoBuscarCisternaFragment.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.javierpinya.testcamiones_v3.AppDatabase;
 import com.javierpinya.testcamiones_v3.Clases.TacsecoEntity;
@@ -26,7 +29,11 @@ public class ResultadoBuscarCisternaFragment extends Fragment {
     private String cisterna;
     private TacsecoEntity tacsecoEntity;
     private TplcprtEntity tplcprtEntity;
-    //private BuscarAsyncTask buscarAsyncTask;
+    private TextView matricula,tipo,chip,adr,itv,ejes,tara,mma,contador,tablacal,sologas,pesados,bloqueada;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    //private BuscarTacsecoAsyncTask buscarAsyncTask;
 
 
     public ResultadoBuscarCisternaFragment(String cisterna) {
@@ -40,25 +47,34 @@ public class ResultadoBuscarCisternaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_resultado_buscar_cisterna, container, false);
-
-
-
+        matricula = view.findViewById(R.id.tv_cisternamatricula);
+        tipo = view.findViewById(R.id.tv_tipocisterna1);
+        chip = view.findViewById(R.id.tv_chipcisterna1);
+        adr = view.findViewById(R.id.tv_adrcisterna1);
+        itv = view.findViewById(R.id.tv_itvcisterna1);
+        ejes = view.findViewById(R.id.tv_ejescisterna1);
+        tara = view.findViewById(R.id.tv_taracisterna1);
+        mma = view.findViewById(R.id.tv_mmacisterna1);
+        contador = view.findViewById(R.id.tv_contadorcisterna1);
+        tablacal = view.findViewById(R.id.tv_tablacalcisterna1);
+        sologas = view.findViewById(R.id.tv_sologasoleoscisterna1);
+        pesados = view.findViewById(R.id.tv_cargapesadoscisterna1);
+        bloqueada = view.findViewById(R.id.tv_Bloqueadacisterna1);
+        mRecyclerView = view.findViewById(R.id.rv_compartimentos);
         return view;
     }
 
-    private class BuscarAsyncTask extends AsyncTask<Void, Integer, TacsecoEntity> {
+    private class BuscarTacsecoAsyncTask extends AsyncTask<Void, Integer, TacsecoEntity> {
 
         @Override
         protected TacsecoEntity doInBackground(Void... voids) {
             tacsecoEntity = AppDatabase.getDatabase(getActivity()).tacsecoDao().findTacsecoByOneMatricula(cisterna);
-            tplcprtEntity = AppDatabase.getDatabase(getActivity()).tplcprtDao().findTplcprtByMatricula(cisterna);
             return tacsecoEntity;
         }
 
 
         @Override
         protected void onPostExecute(TacsecoEntity tacsecoEntity) {
-            /*
 
                 matricula.setText(tacsecoEntity.getMatricula());
                 tipo.setText(tacsecoEntity.getTipo());
@@ -67,10 +83,24 @@ public class ResultadoBuscarCisternaFragment extends Fragment {
                 itv.setText(tacsecoEntity.getFec_cadu_itv().toString());
                 tara.setText(tacsecoEntity.getTara());
                 mma.setText(tacsecoEntity.getPeso_maximo());
-                transresp.setText("-");
-                bloqueado.setText(String.valueOf(tacsecoEntity.getInd_bloqueo()));
+                bloqueada.setText(String.valueOf(tacsecoEntity.getInd_bloqueo()));
+                tablacal.setText(tacsecoEntity.getFec_cadu_calibracion().toString());
+                sologas.setText(String.valueOf(tacsecoEntity.getInd_bloqueo()));
+                pesados.setText(String.valueOf(tacsecoEntity.isInd_carga_pesados()));
+        }
+    }
 
-             */
+    private class BuscarTplcprtAsyncTask extends AsyncTask<Void,Integer,TplcprtEntity>{
+
+        @Override
+        protected TplcprtEntity doInBackground(Void... voids) {
+            tplcprtEntity = AppDatabase.getDatabase(getActivity()).tplcprtDao().findTplcprtByMatricula(cisterna);
+            return tplcprtEntity;
+        }
+
+        @Override
+        protected void onPostExecute(TplcprtEntity tplcprtEntity) {
+            super.onPostExecute(tplcprtEntity);
 
         }
     }
